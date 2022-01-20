eeb1707f4a4ba9608d45b83c0be6f8f05305e741
SuperTikuwa <tikuwamk2@gmail.com>
2022-01-21 00:21:24 +0900

    fix: エンドポイントの不足やレスポンスの整合性について修正

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,11 +13,22 @@ router.post("/", async (req, res, next) => {
   try {
     await db.users.create({ name: req.query.name });
   } catch (e) {
+    console.log(e);
     res.status(400).send("User already exists");
     return;
   }
 
-  res.status(201).send();
+  const u = await db.users.findOne({ where: { name: req.query.name } });
+
+  res.status(201).send({ id: u.id });
+});
+
+router.post("/admin", (req, res, next) => {
+  if (req.body.password === "admin") {
+    res.status(200).send();
+    return;
+  }
+  res.status(401).send();
 });
 
 module.exports = router;
